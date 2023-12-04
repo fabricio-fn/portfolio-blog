87058cca30d863de764a012cc487e3fcaa5a00fa
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-12-04 18:02:28 -0300

    ✨ feat: Add 'types' folder for type definitions

---
+++ src/types/interfaces.ts
@@ -0,0 +1,25 @@
+import { PortableTextBlock } from 'sanity';
+
+export interface IPost {
+  _id: string;
+  title: string;
+  slug: { current: string };
+  mainImage: {
+    alt: string;
+    asset: { url: string };
+  };
+  publishedAt: string;
+  body: PortableTextBlock[];
+}
+
+export interface ISkill {
+  icon: React.ReactElement;
+  label: string;
+  name: string;
+}
+
+export interface ISocialMedia {
+  icon: React.ReactElement;
+  label: string;
+  href: string;
+}
