e902e0cf1670d5859f4a7206b269394beb7bf1e6
Fabricio Nascimento <fabriciocunha040@gmail.com>
2023-12-12 17:41:31 -0300

    :label: feat: Add additional types for PortableTextComponents

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -30,3 +30,51 @@ export interface IProjects {
   link: string;
   code: string;
 }
+
+// PortableTextComponents
+
+interface ImageComponentProps {
+  value: {
+    alt: string;
+  };
+}
+
+interface CodeComponentProps {
+  value: {
+    filename?: string;
+    language: string;
+    code: string;
+  };
+}
+
+interface BlockComponentProps {
+  children: React.ReactNode;
+}
+
+interface LinkComponentProps {
+  value: { href: string };
+  children: React.ReactNode;
+}
+
+export interface IPortableTextComponents {
+  image: React.FC<ImageComponentProps>;
+  types: {
+    code: React.FC<CodeComponentProps>;
+  };
+  block: {
+    h2: React.FC<BlockComponentProps>;
+    h3: React.FC<BlockComponentProps>;
+    h4: React.FC<BlockComponentProps>;
+    normal: React.FC<BlockComponentProps>;
+    blockquote: React.FC<BlockComponentProps>;
+  };
+  list: {
+    bullet: React.FC<BlockComponentProps>;
+    number: React.FC<BlockComponentProps>;
+  };
+  marks: {
+    strong: React.FC<BlockComponentProps>;
+    emphasis: React.FC<BlockComponentProps>;
+    link: React.FC<LinkComponentProps>;
+  };
+}
